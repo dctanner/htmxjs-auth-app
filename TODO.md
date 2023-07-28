9f2997ebb7fac98a6489e9871adabb48aa5efb8b
Damien Tanner <damien@tanner.me>
2023-07-28 12:07:53 +0100

    done todos

--- TODO.md
+++ TODO.md
@@ -1,1 +1,1 @@
-Fork stu's cloudflare-auth into htmxjs org, migrate it to new struture and then copy views and email code from here into it
+
--- app/lib/zodSchema.js
+++ app/lib/zodSchema.js
@@ -1,6 +1,5 @@
 import { z } from 'zod'
 
-// TODO verify email format
 export const userSchema = z.object({
   email: z.string().email(),
 })
